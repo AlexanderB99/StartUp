62cdbaa779dfdb998b5555868f039f64a05c258c
Trolul <eusebiu.buga@gmail.com>
2020-02-27 19:31:16 +0100

    Added simple building system

--- RealeaseGame/Intermediate/Build/Win64/RealeaseGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ RealeaseGame/Intermediate/Build/Win64/RealeaseGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 #include "D:/startup/StartUp/RealeaseGame/Intermediate/Build/Win64/RealeaseGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
 =======
 #include "E:/BaseDefenseGit/StartUp/RealeaseGame/Intermediate/Build/Win64/RealeaseGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
 >>>>>>> cb36693... Imported models and textures
+=======
+#include "D:/Project 3rd quartile/RealeaseGame/Intermediate/Build/Win64/RealeaseGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+>>>>>>> 3f755f4... Added simple building system
